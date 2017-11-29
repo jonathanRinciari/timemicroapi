b53b6935f4ee812935f731d7c286e0266e98373a
jon <jrinciari@or-specialties.com>
2017-11-29 17:44:33 +0000

    updated index.html

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 var express = require("express"),
     app = express(),
     moment = require('moment');
-    
-app.use(express.static('public'))
 
-app.get('/:id', function(req, res){
-    var time = req.params.id
-    if(moment(time, 'MMMM-DD-YYYY')){
-        res.send(time)
-    } else {
-        res.send('nope')
-    }
+app.use(express.static('public'))
+app.get('/', function(req, res){
+    res.sendFile('index.html')
+})
+
+app.get('/:timestamp', function(req, res){
+    var inputDate;
+   if(/^\d{8,}$/.test(req.params.timestamp)){
+       inputDate = moment(req.params.timestamp, 'X');
+   } else {
+       inputDate = moment(req.params.timestamp, "MMMM D, YYYY");
+   }
+
+   if(inputDate.isValid()){
+       res.json({
+           unix: inputDate.format("X"),
+           natural: inputDate.format("MMMM D, YYYY")
+       })
+   } else{
+       res.json({
+           unix: null,
+           natural: null
+       })
+   }
 })
 
 app.listen(process.env.PORT, process.env.IP, function(){
-  console.log("Testing")  
-})ing")  
+  console.log("Testing");
 })
